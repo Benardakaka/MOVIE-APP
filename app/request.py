9105c23ccba5ec20a2970f20c04ea8c80232dee7
Benardakaka <benardakaka484@gmail.com>
2020-12-10 13:14:53 +0300

    add

--- app/request.py
+++ app/request.py
@@ -74,11 +74,11 @@ def get_movie(id):
     return movie_object
 
 def search_movie(movie_name):
-    search_movie_url = 'https://api.themoviedb.org/3/search/movie?api_key={}&query={}'.format(api_key,movie_name)
+    search_movie_url = 'https://api.themoviedb.org/3/search/movie?api_key=7ef2476e485cb858f1c9ab9acea947f3&query={}'.format(movie_name)
     with urllib.request.urlopen(search_movie_url) as url:
         search_movie_data = url.read()
         search_movie_response = json.loads(search_movie_data)
-
+        # print(search_movie_response)
         search_movie_results = None
 
         if search_movie_response['results']:
